6b76989bb6b670842e8bcb855e1b93ecde9e8047
Shahrukh Imran <ShahrukhImran@192.168.1.124>
2021-07-08 13:02:52 +0500

    install deps and set-up guardian for auth

---
+++ lib/guardian.ex
@@ -0,0 +1,22 @@
+defmodule AuthJwtGuardianWeb.Guardian do
+  use Guardian, otp_app: :auth_jwt_guardian
+
+  def subject_for_token(user, _claims) do
+    sub = to_string(user.id)
+    {:ok, sub}
+  end
+
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
+
+  def resource_from_claims(claims) do
+    id = claims["sub"]
+    resource = AuthJwtGuardianWeb.Users.get_user!(id)
+    {:ok,  resource}
+  end
+
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
+  end
+end
--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule AuthJwtGuardian.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
 
-
+      {:comeonin, "~> 4.0"},
+      {:bcrypt_elixir, "~> 1.0"},
+      {:guardian, "~> 2.0"}
     ]
   end
 
